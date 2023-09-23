9cfdb04d6cecd3cb91f1c96059514c2103997adb
Nikhil N <117642368+UserNikhil@users.noreply.github.com>
2023-09-23 18:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your code here
+cy.get("div").then((span) => {
+  const text = span.text().trim();
+  cy.log(text); // Log the actual text
+  expect(text).to.eq(`Welcome to the world of Web development`);
+});
